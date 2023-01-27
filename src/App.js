367ca202f8d1a7f2ff4d4a2d90c79b774318d2d9
Chris Campbell <christopherecampbell@gmail.com>
2023-01-27 16:54:32 -0700

    stub up starships page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
-import Header from './Header'
 import StarShipsIndex from './StarShipsIndex'
+import StarshipPage from './StarshipPage';
 
 function App() {
   return (
     <>
     <Routes>
       <Route path='/' element={<StarShipsIndex />}/>
+      <Route path='/ship' element={<StarshipPage />}/>
     </Routes>
     </>
   );
---
+++ src/StarshipPage.jsx
@@ -0,0 +1,13 @@
+const StarshipPage = () => {
+  return (  
+    <>
+    <h1>This is a starship Page!!!</h1>
+    <div>
+
+    </div>
+    </>
+    
+  )
+}
+
+export default StarshipPage;
